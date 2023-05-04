f0ddaf61f474345c960058bde9ce8b59f00909db
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-05-04 21:03:52 +0300

    Finished

--- src/components/SectorsComponents/SectorsHeader.jsx
+++ src/components/SectorsComponents/SectorsHeader.jsx
@@ -21,19 +21,19 @@ const SectorsHeader = ({ setSelectedsector, selectedsector }) => {
   ];
   return (
     <div
-      className="flex md:p-8 p-2 flex-col  bg-cover bg-center bg-no-repeat h-[200px]"
+      className="flex md:p-8 p-2 flex-col  bg-cover bg-center bg-no-repeat h-[250px] md:h-[200px]"
       style={{
         backgroundImage: `url(${sectorsheaderbg})`,
       }}
     >
       <h1 className="text-white text-5xl font-bold">GS1 Sectors</h1>
-      <div className="flex justify-between py-8 items-center">
+      <div className="flex flex-wrap justify-between md:py-8 py-2 items-center">
         {sectors.map((sector) => (
           <div
             className={
               selectedsector === sector.name
-                ? "  bg-[#F26334] text-white md:w-[123px] w-[100%] md:h-[60px] h-[30px] cursor-pointer  rounded-lg flex  items-center justify-center   p-4"
-                : "bg-white md:w-[123px] w-[100%] gap-2 md:h-[60px] h-[30px] cursor-pointer  rounded-lg flex items-center justify-center p-4  "
+                ? "  bg-[#F26334] text-white md:w-[123px] my-2 w-[180px]  md:h-[60px]  cursor-pointer  rounded-lg flex  items-center justify-center   p-4"
+                : "bg-white md:w-[123px] w-[180px] my-2 gap-2 md:h-[60px]  cursor-pointer  rounded-lg flex items-center justify-center p-4  "
             }
             onClick={() => setSelectedsector(sector.name)}
           >
